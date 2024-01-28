34caadb99fdcde671aa9a9294431f3f36743c34b
Mark <mkstreet.bangkok@gmail.com>
2024-01-28 16:38:19 +0700

    Changes to be committed: 	new file:   classanimal.py

---
+++ classanimal.py
@@ -0,0 +1,18 @@
+
+class Animal:
+
+        _animalname = None
+
+
+
+        def __init(self, animalname:str):
+                self._animalname = animalname
+
+                return
+        
+        def moves(self):
+                return None
+        
+        def sound(self):
+                return None
+        
